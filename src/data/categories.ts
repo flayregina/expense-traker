6488e75cdceaa633f7e6f0b7d2b02e9dd9777baa
Flavia Regina <flaviafreire23@gmail.com>
2022-05-16 17:03:17 -0300

    Projeto Gerenciador de despesas

---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+import { Category } from "../types/Category"
+
+export const categories: Category = {
+    energy: { title: 'Energia', color: 'blue', expense: true },
+    Water: { title: 'Água', color: 'brown', expense: true },
+    supermarket: { title: 'Supermercado', color: 'purple', expense: true },
+    salary: { title: 'Salário', color: 'green', expense: false }
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item"
+
+export const items: Item[] = [
+    { date: new Date(2022, 4, 15), category: 'energy', title: 'Equatorial energia', value: 190 },
+    { date: new Date(2022, 4, 15), category: 'Water', title: 'Aguas de Teresina', value: 102 },
+    { date: new Date(2022, 4, 10), category: 'supermarket', title: 'Atacadão', value: 190 },
+    { date: new Date(2022, 5, 9), category: 'salary', title: 'Salário Davenstall', value: 6000 }
+]
